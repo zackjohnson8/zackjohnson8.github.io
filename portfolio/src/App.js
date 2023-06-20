56b2afef2481790ee9585273ead7ce5371797965
zackjohnson8 <zackjohnson8@gmail.com>
2023-06-20 12:34:46 -0700

    Added picture to center of website

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import staticphoto from './BusinessCas1to1.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={staticphoto} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
