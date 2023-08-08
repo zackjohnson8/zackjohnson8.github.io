f68789c21860602e9fa3bb9699ae3ed894f09bd5
zackjohnson8 <zackjohnson8@gmail.com>
2023-08-07 20:23:08 -0700

    - Updated About.js

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -10,7 +10,7 @@ function About() {
                 <img src={photo} className="Headshot" alt="logo" />
                 <div>
                     <p className="EntryText">
-                        A highly skilled and solution-oriented Software Engineer with 3 years of experience in designing, developing, and maintaining software applications. Proficient in a broad range of programming languages, including Python, C#, and Swift. Demonstrated expertise in diverse areas such as web development, automation, iOS development, and DevOps, as well as both front-end and back-end architectures. Recognized for the ability to quickly learn and master new technologies, producing clean,  efficient code, and driving projects to successful completion. Committed to achieving technical excellence, eager to contribute to team successes, and passionate about continuous learning and professional growth.
+                        A skilled and solution-oriented Software Engineer with 3 years of experience in designing, developing, and maintaining software applications. Proficient in a broad range of programming languages, including Python, C#, and Swift. Demonstrated expertise in diverse areas such as web development, automation, iOS development, and DevOps, as well as both front-end and back-end architectures. Recognized for the ability to quickly learn and master new technologies, producing clean, efficient code, and driving projects to successful completion. Committed to achieving technical excellence, eager to contribute to team successes, and passionate about continuous learning and professional growth.
                     </p>
                 </div>
             </header>
